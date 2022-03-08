5cacee8d266982a70b38657e2ae4f983dacd84de
uyamazak <yu.yamazaki85@gmail.com>
2022-03-08 10:05:48 +0900

    add false pattern

--- middleware/test-guard.ts
+++ middleware/test-guard.ts
@@ -1,9 +1,10 @@
-const isNumberParam = (param: string) => {
-  return !isNaN(Number(param)) && Number(param) > 0
-}
+
 export default defineNuxtRouteMiddleware((to, from) => {
   const { testVal } = to.params
-  if (testVal === 'test' ) {
-    return abortNavigation('testVal is test')
+  if (testVal === 'abort' ) {
+    return abortNavigation('testVal is abort')
+  }
+  if (testVal === 'false' ) {
+    return abortNavigation('testVal is false')
   }
 })
